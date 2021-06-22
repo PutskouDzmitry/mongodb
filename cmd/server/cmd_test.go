132e10d85cffb7e390a79e8a61f35428b94b146f
PutskouDzmitry <70838579+PutskouDzmitry@users.noreply.github.com>
2021-06-22 15:36:15 +0300

    Update cmd_test.go

--- cmd/server/cmd_test.go
+++ cmd/server/cmd_test.go
@@ -14,6 +14,18 @@ func getOneStr(str string, lenStrInt int) string{
 	return strArray[lenStr][lenStrInt:]
 }
 
+func Test_Server_Add(t *testing.T){
+	assert := assert.New(t)
+	expectedId := "ObjectID(\\\"68c3503f9a9e8b7cdfa813d4\\\")\"\n"
+	cmd := exec.
+		Command("/bin/sh", "-c", "curl -i -X POST -H 'Content-Type: application/json' -d '{\"BookId\":\"68c3503f9a9e8b7cdfa813d4\",\"AuthorId\":1,\"BookVolume\":2,\"NameOfBook\":\"qwe\",\"Number\":1,\"PublisherId\":2,\"YearOfPublication\":\"2020-12-2\"}' http://localhost:8081/books\n")
+	cmd.Run()
+	actualByte, _ := exec.Command("/bin/sh", "-c", "docker logs db-book").CombinedOutput()
+	actualStr := string(actualByte)
+	finalStr := getOneStr(actualStr, 39)
+	assert.Equal(expectedId, finalStr)
+}
+
 func Test_Server_ReadAll(t *testing.T) {
 	assert := assert.New(t)
 	command := "curl http://localhost:8081/books"
@@ -40,17 +52,6 @@ func Test_Server_ReadAllNotEqual(t *testing.T) {
 	assert.NotEqual(expected, finalStr)
 }
 
-func Test_Server_Add(t *testing.T){
-	assert := assert.New(t)
-	expectedId := "ObjectID(\\\"68c3503f9a9e8b7cdfa813d4\\\")\"\n"
-	cmd := exec.
-		Command("/bin/sh", "-c", "curl -i -X POST -H 'Content-Type: application/json' -d '{\"BookId\":\"68c3503f9a9e8b7cdfa813d4\",\"AuthorId\":1,\"BookVolume\":2,\"NameOfBook\":\"qwe\",\"Number\":1,\"PublisherId\":2,\"YearOfPublication\":\"2020-12-2\"}' http://localhost:8081/books\n")
-	cmd.Run()
-	actualByte, _ := exec.Command("/bin/sh", "-c", "docker logs db-book").CombinedOutput()
-	actualStr := string(actualByte)
-	finalStr := getOneStr(actualStr, 39)
-	assert.Equal(expectedId, finalStr)
-}
 
 func Test_Server_Read(t *testing.T) {
 	assert := assert.New(t)
